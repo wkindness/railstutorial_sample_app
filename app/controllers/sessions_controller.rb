dad08389520728286f402c27ac03d1feba28a048
t kanai <takatoshi.kanai@gmail.com>
2019-05-05 22:08:38 +0900

    Implement advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user && user.authenticate(params[:session][:password])
-      log_in user #sessions_helper.rb
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user #sessions_helper.rb
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new'
@@ -15,7 +16,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out #sessions_helper.rb
+    log_out if logged_in? #sessions_helper.rb
     redirect_to root_url
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,16 @@
 class User < ApplicationRecord
+  # インスタンス変数remember_tokenを外部から変更できるように
+  attr_accessor :remember_token
   # emailは登録時に小文字に変換
   before_save { self.email = email.downcase }
-
   # nameは必須、50文字まで
   validates :name,  presence: true, length: { maximum: 50 }
-
   # emailは必須、255文字まで、メールアドレス形式、重複禁止（大小区別なし）
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
                     length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-
   # セキュアなpassword
   has_secure_password
   # passwordは必須、6文字以上
@@ -22,6 +21,28 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # ランダムなトークンを返す
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # 永続セッションのためにユーザーをデータベースに記憶する
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # 渡されたトークンがダイジェストと一致したらtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーのログイン情報を破棄する
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
 end
